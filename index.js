cc0c9988d9058de2d5a27f47a605619edd9996ef
mdharoonhussain <haroonhussain97@gmail.com>
2023-12-18 19:29:45 +0530

    final commit

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const express = require("express");
+const axios = require("axios");
+const cron = require("node-cron");
 const {connection} = require("./connection/db");
 const router = require("./routes/route");
-const app = express()
 require("dotenv").config();
-// const PORT = 4500;
 connection();
+
+const app = express()
 app.use(express.json());
+
+const swapiURL = "https://swapi.dev/api/people/";
+const cronSchedule = "*/5 * * * *";
+
+// Function to ping the SWAPI API
+const pingSWAPI = async () => {
+  try {
+    const res = await axios.get(swapiURL);
+    console.log(`Ping successful! Status Code: ${res.status}`);
+  } catch (error) {
+    console.log(`Ping failed! Error: ${error.message}`);
+  }
+};
+
+// Set up the cron job
+cron.schedule(cronSchedule, () => {
+  console.log(`Running SWAPI ping cron job at ${new Date().toLocaleString()}`);
+  pingSWAPI();
+});
 
 app.use("/api", router);
 
-// app.get("/",(req,res)=>{
-//     res.status(201).send("Hello World");
-// })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async() => {
+  try {
+    await connection;
+    console.log("Database is connected");
+  } catch (error) {
+    console.log(error);
+    console.log("Database is not connected");
+  }
     console.log(`Server is running at port ${process.env.PORT}`);
   });
